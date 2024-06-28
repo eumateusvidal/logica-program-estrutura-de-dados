58ad6b80e3405aff910513f70f6e44c36c303d4f
Mateus Vidal <devmateusvidal@gmail.com>
2024-06-27 21:25:46 -0300

    course update

--- introducao/introducao.js
+++ introducao/introducao.js
@@ -1,1 +1,12 @@
-alert ("Olá de arquivo externo!!")
+let items = ["apple", "banana", "cherry"];
+for (let i = 0; i < items.length; i++) {
+    console.log(items[i]);
+}
+
+// Outro exemplo comum: Contagem regressiva
+let countdown = 10;
+while (countdown > 0) {
+    console.log(countdown);
+    countdown--;
+}
+console.log("Happy New Year!");
