68a53ccad52a12531c874cb69788199bc0d871c2
Mateus Vidal <devmateusvidal@gmail.com>
2024-06-30 11:13:26 -0300

    course update

---
+++ variaveis-e-tipos-de-valores/02-exemplo-2.js
@@ -0,0 +1,17 @@
+let numero1 = prompt ('Digite um número')
+numero1 = parseFloat(numero1)
+
+let numero2 = prompt ('Digite outro número')
+numero2 = parseFloat (numero2)
+
+let soma = numero1 + numero2
+
+/* Como converter comando em número?
+
+number(), parseInt(), parseFloat()
+
+*/
+
+//alert ('A soma é: ' + soma)
+
+alert ('A soma é: '+ (numero1 + numero2) )
---
+++ variaveis-e-tipos-de-valores/03-primeirodesafio.js
@@ -0,0 +1,37 @@
+// Pedir para o usuário digitar um número
+
+// converter em número inteiro
+
+// Pedir para o usuário digitar outro número
+
+// converter em número inteiro
+
+// mostrar a divisão contatenando com o texto "A divisão é ..."
+
+// mostrar a multiplicar contatenando com o texto "A multiplicação é ..."
+
+// mostrar a subtração contatenando com o texto "A subtração é ..."
+
+// mostrar a soma contatenando com o texto "A soma é ..."
+
+let number1  = prompt ('Digite aqui um número')
+number1 = parseInt (number1)
+
+let number2  = prompt ('Digite aqui outro número')
+number2 = parseInt (number2)
+
+/*  let dividir = number1 / number2
+
+let multiplicar = number1 * number2
+
+let subtrair = number1 - number2
+
+let somar = number1 + number2 */
+
+alert ('A divisão é: ' + (number1 / number2));
+
+alert ('A multiplicação é: ' + (number1 * number2));
+
+alert ('A subtração é: ' + (number1 - number2));
+
+alert ('A soma é: ' + (number1 + number2));
