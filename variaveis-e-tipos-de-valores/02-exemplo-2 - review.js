413ee8613206c5b850a19ad1c06c7035ff148b53
Mateus Vidal <devmateusvidal@gmail.com>
2024-09-30 05:39:42 -0300

    daily update

--- variaveis-e-tipos-de-valores/02-exemplo-2 - review.js
+++ variaveis-e-tipos-de-valores/02-exemplo-2 - review.js
@@ -4,5 +4,12 @@ numero1 = parseFloat(numero1)
 let numero2 = prompt ('Digite outro número')
 numero2 = parseFloat(numero2)
 
+// Number(), parseInt(), parseFloat()
+
+let soma = numero1 + numero2
+
 // alert('A soma dos dois números é')
-alert(numero1 + numero2)
+// alert("A soma é: " + (numero1 + numero2))
+// Os parenteses internos são executados com prioridade
+
+alert("A soma é: " + soma)
