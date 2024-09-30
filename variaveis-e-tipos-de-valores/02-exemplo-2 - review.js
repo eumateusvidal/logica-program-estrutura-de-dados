33f7134b9ea4fb046d1013835b23fdf11ae2ab48
Mateus Vidal <devmateusvidal@gmail.com>
2024-09-30 05:05:15 -0300

    september 30

---
+++ variaveis-e-tipos-de-valores/02-exemplo-2 - review.js
@@ -0,0 +1,8 @@
+let numero1 = prompt ('Digite um número')
+numero1 = parseFloat(numero1)
+
+let numero2 = prompt ('Digite outro número')
+numero2 = parseFloat(numero2)
+
+// alert('A soma dos dois números é')
+alert(numero1 + numero2)
