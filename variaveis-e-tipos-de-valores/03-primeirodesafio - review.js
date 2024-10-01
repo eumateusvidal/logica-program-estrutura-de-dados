89ef09146d7459bdd67affd6f77d6d29827dcd8e
Mateus Vidal <devmateusvidal@gmail.com>
2024-10-01 05:17:44 -0300

    october 1

--- variaveis-e-tipos-de-valores/03-primeirodesafio - review.js
+++ variaveis-e-tipos-de-valores/03-primeirodesafio - review.js
@@ -1,34 +1,27 @@
 // Pedir para o usuário digitar um número
+let nu1 = prompt('Digite um número')
 
 // converter em número inteiro
+nu1 = parseInt(nu1)
 
 // Pedir para o usuário digitar outro número
+let nu2 = prompt('Digite outro número')
 
 // converter em número inteiro
+nu2 = parseInt(nu2)
 
 // mostrar a divisão contatenando com o texto "A divisão é ..."
+alert('A divisão é: ' + (nu1 / nu2))
 
 // mostrar a multiplicar contatenando com o texto "A multiplicação é ..."
+alert('A multiplicação é: ' + (nu1 * nu2))
 
 // mostrar a subtração contatenando com o texto "A subtração é ..."
+alert('A subtração é: ' + (nu1 - nu2))
 
 // mostrar a soma contatenando com o texto "A soma é ..."
+alert('A soma é: ' + (nu1 + nu2))
 
 // let n1 = parseFloat(Prompt ('Digite um número'))
 
 // let n2 = parseFloat(prompt('Digite outro número'))
-
-
-let nu1 = prompt('Digite um número')
-nu1 = parseInt(nu1)
-
-let nu2 = prompt('Digite outro número')
-nu2 = parseInt(nu2)
-
-divisao = nu1 / nu2
-
-multiplicacao = nu1 * nu2
-
-alert('A divisão é: ' + divisao)
-
-alert('A divisão é: ' + multiplicacao)
