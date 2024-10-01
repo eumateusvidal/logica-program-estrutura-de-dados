27ac0cde7541313f9d047929a3811c8e5983726a
Mateus Vidal <devmateusvidal@gmail.com>
2024-10-01 05:51:06 -0300

    october 1 up

---
+++ variaveis-e-tipos-de-valores/05.terceirodesafio.js
@@ -0,0 +1,11 @@
+// pedir para o usuário dois números
+let n1 = prompt('Digite aqui seu número')
+n1 = parseFloat (n1)
+
+let n2 = parseFloat(prompt('Digite um número'))
+
+// calcular a média
+let media = (n1 + n2) / 2
+
+//mostrar a média na tela
+alert('A média é:' + media)
