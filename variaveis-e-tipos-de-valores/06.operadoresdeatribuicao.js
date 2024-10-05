6b16de5f4403b8ef6261fab8f5867ec8ca1264c3
Mateus Vidal <devmateusvidal@gmail.com>
2024-10-05 17:22:50 -0300

    daily update

--- variaveis-e-tipos-de-valores/06.operadoresdeatribuicao.js
+++ variaveis-e-tipos-de-valores/06.operadoresdeatribuicao.js
@@ -1,12 +1,22 @@
-let x = 10
-x = 10 + 20
-30
+let number01 = parseFloat (Prompt('Digite um número'))
 
-x = x + 50
-80
+let number02 = parseFloat (Prompt('Digite outro número'))
 
-x = 30
-x += 50
+let soma = number01 + number01
+
+alert ('O total da soma é: ' + soma)
+
+// let x = 10
+// x = 10 + 20
+// 30
+
+// x = x + 50
+// 80
+
+// x = 30
+// x += 50
+
+
 
 
 
