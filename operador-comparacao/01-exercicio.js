9525389f03fb4f92f89c123488f4a61e8922c021
Mateus Vidal <devmateusvidal@gmail.com>
2024-10-15 07:01:26 -0300

    october 15

---
+++ operador-comparacao/01-exercicio.js
@@ -0,0 +1,5 @@
+let n1 = parseFloat (prompt("Digite um número de 0 a 10"))
+
+let n2 = parseFloat (prompt('Digite outro número de 0 a 10'))
+
+alert ('O segundo número é maior ou igual ao primeiro? ' + (n2 >= n1))
---
+++ operador-comparacao/02-exercicio.js
@@ -0,0 +1,7 @@
+let n1 = parseFloat (prompt("Digite um número de 0 a 10"))
+
+let n2 = parseFloat (prompt('Digite outro número de 0 a 10'))
+
+let media = (n1 + n2) / 2
+
+alert ('A média é maior que 7? ' + (media == 7))
